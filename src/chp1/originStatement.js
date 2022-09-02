aaba9fc8208c9056b5f0bfb7672d72ff72b41a0e
Conquerex <conquerex@gmail.com>
2022-09-03 00:43:43 +0900

    p32

--- src/chp1/originStatement.js
+++ src/chp1/originStatement.js
@@ -13,27 +13,7 @@ function myStatement(invoice, plays) {
 
     for (let perf of invoice.performances) {
         const play = plays[perf.playID];
-        let thisAmount = 0;
-
-        switch (play.type) {
-            case "tragedy": // 비극
-                thisAmount = 40000;
-                if (perf.audience > 30) {
-                    thisAmount += 1000 * (perf.audience - 30);
-                }
-                break;
-
-            case "comedy": // 희극
-                thisAmount = 30000;
-                if (perf.audience > 20) {
-                    thisAmount += 10000 + 500 * (perf.audience - 20);
-                }
-                thisAmount += 300 * perf.audience;
-                break;
-
-            default:
-                throw new Error(`알 수 없는 장르: ${play.type}`)
-        }
+        let thisAmount = amountFor(perf, play);
 
         // 포인트 적립
         volumeCredits += Math.max(perf.audience - 30, 0);
