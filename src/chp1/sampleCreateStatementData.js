504bb84353281ed5f5da52fbb6abe7b635aff4f9
Conquerex <conquerex@gmail.com>
2022-09-05 00:48:15 +0900

    p70

--- src/chp1/sampleCreateStatementData.js
+++ src/chp1/sampleCreateStatementData.js
@@ -17,54 +17,56 @@ export default function createStatementData(invoice, plays) {
     }
 
     function enrichPerformance(aPerformance) {
+        const calculator = new PerformanceCalculator(aPerformance, playFor(aPerformance));
         const result = Object.assign({}, aPerformance); // 얕은 복사 수행
-        result.play = playFor(result);
-        result.amount = amountFor(result);
-        result.volumeCredits = volumeCreditsFor(result);
-        return result;
-    }
-
-    // 포인트 적립
-    function volumeCreditsFor(aPerformance) {
-        let volumeCredits = 0;
-        volumeCredits += Math.max(aPerformance.audience - 30, 0);
-        // 희극 관객 5명마다 추가 포인트를 제공
-        if ('comedy' === aPerformance.play.type) {
-            volumeCredits += Math.floor(aPerformance.audience / 5);
-        }
-        return volumeCredits;
-    }
-
-    // 값이 변경되지 않는 변수는 매개 변수로 전달
-    function amountFor(aPerformance) {
-        // let thisAmount > let result
-        let result = 0;
-        switch (aPerformance.play.type) {
-            case "tragedy": // 비극
-                result = 40000;
-                if (aPerformance.audience > 30) {
-                    result += 1000 * (aPerformance.audience - 30);
-                }
-                break;
-
-            case "comedy": // 희극
-                result = 30000;
-                if (aPerformance.audience > 20) {
-                    result += 10000 + 500 * (aPerformance.audience - 20);
-                }
-                result += 300 * aPerformance.audience;
-                break;
-
-            default:
-                throw new Error(`알 수 없는 장르: ${aPerformance.play.type}`)
-
-        }
-
-        // 함수 안에서 값이 바뀌는 변수 반환
+        result.play = calculator.play;
+        result.amount = calculator.amount;
+        result.volumeCredits = calculator.volumeCredits;
         return result;
     }
 
     function playFor(aPerformance) {
         return plays[aPerformance.playID];
+    }
+}
+
+class PerformanceCalculator {
+    constructor(aPerformance, aPlay) {
+        this.performance = aPerformance;
+        this.play = aPlay;
+    }
+
+    get volumeCredits() {
+        let result = 0;
+        result += Math.max(this.performance.audience - 30, 0);
+        if ('comedy' === this.play.type) {
+            result += Math.floor(this.performance.audience / 5);
+        }
+        return result;
+    }
+
+    get amount() {
+        let result = 0;
+        switch (this.play.type) {
+            case "tragedy": // 비극
+                result = 40000;
+                if (this.performance.audience > 30) {
+                    result += 1000 * (this.performance.audience - 30);
+                }
+                break;
+
+            case "comedy": // 희극
+                result = 30000;
+                if (this.performance.audience > 20) {
+                    result += 10000 + 500 * (this.performance.audience - 20);
+                }
+                result += 300 * this.performance.audience;
+                break;
+
+            default:
+                throw new Error(`알 수 없는 장르: ${this.play.type}`)
+        }
+
+        return result;
     }
 }
